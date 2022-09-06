6eeeceb592e3d91bf6fa0e2d11c550876059c127
MateoPCMesa <realmateox22@gmail.com>
2022-09-06 01:00:22 -0500

    intento de pruebas

--- scrypt.js
+++ scrypt.js
@@ -1,13 +1,29 @@
-let cont = document.getElementById("contador");
+function iniciar (){
+    var cont = document.getElementById("contador");
+    const aumentara = document.getElementById("aumentar");
+    const disminuira = document.getElementById("disminuir");
+    
+    aumentara.addEventListener.apply("click", aumentar(cont));
+    disminuira.addEventListener.apply("click", disminuir(cont));
+    
+   
 
-
-
-const aumentar = document.getElementById("aumentar");
-const disminuir = document.getElementById("disminuir");
-
-aumentar.addEventListener("click", aumentar);
-
-function aumentar() {
-    let temp = cont.innerHTML;
-    cont.innerHTML = parseInt(temp) + 1;
 }
+
+function aumentar(cont) {
+    let temp = cont.innerHTML;
+    cont.innerHTML = (parseInt(temp)+1);
+    console.log(cont.innerHTML);
+    console.log("aumentar");
+
+}
+function disminuir(cont) {
+      let temp = cont.innerHTML;
+    cont.innerHTML = (parseInt(temp)-1);
+    console.log("disminuir");
+    console.log(cont.innerHTML);
+
+}
+
+
+
